90edc487fe292762ac8960badbea09948cb79c62
Kostin333 <kostin333@yandex.ru>
2024-08-06 23:35:15 +0300

    new

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, PostSearch, PostCreate, PostUpdate, PostDelete
 
 urlpatterns = [
-   path('', PostList.as_view()),
-
-   path('<int:id>', PostDetail.as_view()),]
+   path('news/', PostList.as_view(), name='post_list'),
+   path('news/<int:id>/', PostDetail.as_view(), name='post_detail'),
+   path('news/search/', PostSearch.as_view(), name='post_search'),
+   path('news/create/', PostCreate.as_view(), name='news_create'),
+   path('news/<int:pk>/edit/', PostUpdate.as_view(), name='news_edit'),
+   path('news/<int:pk>/delete/', PostDelete.as_view(), name='news_delete'),
+   path('articles/create/', PostCreate.as_view(), name='article_create'),
+   path('articles/<int:pk>/edit/', PostUpdate.as_view(), name='article_edit'),
+   path('articles/<int:pk>/delete/', PostDelete.as_view(), name='article_delete'),
+]
